a6674d7faf48fda562b45fd6d5203a90ffde5151
default <nicholas.sun@stu.ocsb.ca>
2025-11-19 15:13:45 +0000

    Changed "var" to "let"

--- main.ts
+++ main.ts
@@ -6,10 +6,10 @@
  */
 
 // Declaring variables
-var radius: number = 5.6;
+let radius: number = 5.6;
 const PI: number = 3.14;
 
-var area: number = PI * radius * radius;
+let area: number = PI * radius * radius;
 
 // Printing
 console.log("A circle with radius of " + String(radius) + " cm has an area of " + String(area) + " cm^2.");
